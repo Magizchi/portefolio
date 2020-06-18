c82f19f98f67d68c90e0016693286fd89a05458d
Rajanan <g.rajan1993@gmail.com>
2020-06-18 22:12:04 +0200

    change meta tag description and icon

---
+++ src/icon/Index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const icon = () => {
+  return (
+    <svg
+      height="30"
+      width="30"
+      fill="currentColor"
+      className="flex items-center justify-center border-2 border-purple-700 rounded-full"
+    >
+      <text x="2" y="18" className="font-bold text-purple-700 bg-purple-700">
+        RG
+      </text>
+    </svg>
+  );
+};
+
+export default icon;
